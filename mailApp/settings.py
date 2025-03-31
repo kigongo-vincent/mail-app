1afe62f7f0d583ae5b56812b221f24700c646044
kigongovincent <kigongovincent81@gmail.com>
2025-03-31 12:20:37 +0300

    code cleanup

--- mailApp/settings.py
+++ mailApp/settings.py
@@ -122,11 +122,13 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+from . import config
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ="kigongovincent81@gmail.com"
-EMAIL_HOST_PASSWORD = "tpdx tffh rlet tovc"
+EMAIL_HOST_USER = config.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
